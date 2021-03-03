0cf3f65541bd19860e8fbc22d4907a0a92ad15c2
Mohamed EL-Khouly <msirag95@gmail.com>
2021-03-03 19:44:13 +0200

    play round added

--- js/app.js
+++ js/app.js
@@ -9,3 +9,32 @@ function computerPlay()
 	result = options[choice];
 	return result;  
 }
+
+// 2. user Play
+
+function userPlay(){
+
+}
+
+// 3. play round
+
+function playRound(playerSelection , computerSelection ){
+	// check for tie 
+	if (playerSelection === computerSelection){
+		return 'Tie';
+	}
+	else{
+		// check player selection
+		switch(playerSelection){
+			case 'rock':
+				return (computerSelection =='scissors')? 'player win':'computer win';
+				break;
+			case 'paper':
+				return (computerSelection =='rock')? 'player win':'computer win';
+				break;
+			case 'scissors':
+				return (computerSelection =='paper')? 'player win':'computer win';
+				break;
+		}
+	}
+}
