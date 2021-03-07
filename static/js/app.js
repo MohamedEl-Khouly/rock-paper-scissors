071b041cc7eaa73a5ad02c45b6f5eb43e0f861d9
Mohamed EL-Khouly <msirag95@gmail.com>
2021-03-07 17:12:26 +0200

    add start button functionality

--- static/js/app.js
+++ static/js/app.js
@@ -2,52 +2,51 @@
 
 // 1. computer play
 
-function computerPlay()
-{
-	options = ['rock','paper','scissors'];
+function computerPlay() {
+	options = ['rock', 'paper', 'scissors'];
 	choice = Math.floor(Math.random() * 3);
 	result = options[choice];
-	return result;  
+	return result;
 }
 
 // 2. user Play
 
-function userPlay(){
+function userPlay() {
 	let choice = prompt('player turn\n enter \n rock, paper or scissors');
 	return choice.toLowerCase()
 }
 
 // 3. play round
 
-function playRound(playerSelection , computerSelection ){
+function playRound(playerSelection, computerSelection) {
 	// check for tie 
-	if (playerSelection === computerSelection){
+	if (playerSelection === computerSelection) {
 		return 'Tie';
 	}
-	else{
+	else {
 		// check player selection
-		switch(playerSelection){
+		switch (playerSelection) {
 			case 'rock':
-				return (computerSelection =='scissors')? 'player':'computer';
+				return (computerSelection == 'scissors') ? 'player' : 'computer';
 				break;
 			case 'paper':
-				return (computerSelection =='rock')? 'player':'computer';
+				return (computerSelection == 'rock') ? 'player' : 'computer';
 				break;
 			case 'scissors':
-				return (computerSelection =='paper')? 'player':'computer';
+				return (computerSelection == 'paper') ? 'player' : 'computer';
 				break;
 		}
 	}
 }
 
 // Main function
-function game(){
+function game() {
 	let playerScore = 0;
 	let computerScore = 0;
 	for (let i = 0; i < 5; i++) {
 		let computerSelection = computerPlay();
 		let playerSelection = userPlay();
-		roundResult = playRound(playerSelection,computerSelection);
+		roundResult = playRound(playerSelection, computerSelection);
 		if (roundResult === 'player') {
 			playerScore++;
 			console.log("You win")
@@ -59,8 +58,16 @@ function game(){
 		} else {
 			console.log("Its a tie")
 			console.log(`${playerSelection} equals ${computerSelection}`)
-		}		
+		}
 		console.log(`player : ${playerScore}\ncomputer : ${computerScore}`);
 	}
 }
 
+//###################################################################################################
+const startBtn = document.getElementById("start-btn");
+const pregameSection = document.getElementById('pre-game');
+const gameSection = document.getElementById('game');
+startBtn.onclick = function (e) {
+	pregameSection.classList.add('hidden');
+	gameSection.classList.remove('hidden');
+};
